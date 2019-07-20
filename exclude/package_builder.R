0208109cc73122efe299c84a0724953cf645d718
moodymudskipper <antoine.fabri@gmail.com>
2019-07-20 20:31:07 +0200

    added use_git

--- exclude/package_builder.R
+++ exclude/package_builder.R
@@ -3,6 +3,7 @@ usethis::use_build_ignore("exclude")
 usethis::use_build_ignore("setops.Rproj")
 usethis::use_package("rlang", "Suggests")
 usethis::use_gpl3_license("GPL-3")
+usethis::use_git()
 
 shell("git remote add origin https://github.com/moodymudskipper/rangeops.git",intern = TRUE)
 shell("git push -u origin master -f",intern = TRUE)
