161cbbbd405171b79a6ddaac2e0d93932ba56cb5
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2019-07-23 11:27:18 +0300

    simplified the comparison operations

---
+++ R/comparison_replace.R
@@ -0,0 +1,55 @@
+#' Replacing Values by Comparison
+#'
+#' Operators for replacing values using the standard comparison operators.
+#'
+#' @param x first element of the operation
+#' @param y second element of the operation
+#' @param value replacement value
+#'
+#' @return \code{x} with values for which the comparisons evaluate to TRUE replaced with \code{value}.
+#'
+#' @seealso \code{`==`}
+#' @name comparison_replace
+NULL
+
+#' @rdname comparison_replace
+#' @export
+`>=<-` <- function(x, y, value){
+  in_replace(x, `>=`, y, value)
+}
+
+#' @rdname comparison_replace
+#' @export
+`><-` <- function(x, y, value){
+  in_replace(x, `>`, y, value)
+}
+
+#' @rdname comparison_replace
+#' @export
+`<=<-` <- function(x, y, value){
+  in_replace(x, `<=`, y, value)
+}
+
+#' @rdname comparison_replace
+#' @export
+`<<-` <- function(x, y, value){
+  # this one needs extra care so standard base::`<<-` still works
+  if (missing(value)) {
+    eval.parent(substitute(.Primitive("<<-")(x, y)))
+  } else {
+    in_replace(x, `<`, y, value)
+  }
+}
+
+#' @rdname comparison_replace
+#' @export
+`==<-` <- function(x, y, value){
+  in_replace(x, `==`, y, value)
+}
+
+#' @rdname comparison_replace
+#' @export
+`!=<-` <- function(x, y, value){
+  in_replace(x, `!=`, y, value)
+}
+
