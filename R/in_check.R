218df5e35650f106dd3a40fc734170184513fc01
Antoine Fabri <antoine.fabri@gmail.com>
2019-08-21 00:08:51 +0200

    support markdown and update doc

--- R/in_check.R
+++ R/in_check.R
@@ -2,18 +2,23 @@
 #'
 #' Operators for checking which values are within a given interval or set.
 #'
-#' Parentheses of each operator define the type of matching template:\cr
-#' \code{\%in\{\}\%} - set of all elements that will be matched against\cr
-#' \code{\%in[]\%} - closed numeric interval\cr
-#' \code{\%in()\%} - open numeric interval\cr
-#' \code{\%in(]\%} - interval that is is open on the left and closed on the right\cr
-#' \code{\%in[)\%} - interval that is closed on the left and open on the right\cr
-#' \code{\%in~\%} - using a regular expression\cr
+#' Parentheses of each operator define the type of matching template:
+#' `%in{}%` - set of all elements that will be matched against
+#' `%in[]%` - closed numeric interval
+#' `%in()%` - open numeric interval
+#' `%in(]%` - interval that is is open on the left and closed on the right
+#' `%in[)%` - interval that is closed on the left and open on the right
+#' `%in~%` - using a regular expression
 #
-#' Operators starting with a bang (i.e. \code{\%out\{\}\%}) specify negation.
+#' Operators of the form `%out<suffix>%` return the negation of `%in<suffix>%`
 #'
-#' The \code{\%in\{\}\%} operator is a reimplementation of \code{\%in\%} that preserves the dimensions of \code{x} (see examples).
-#' Operations using numeric intervals are a convenient short hand form of\cr\code{x > interval[1] & x < interval[2]}.
+#' The `%in{}%` operator is a reimplementation of `%in%` that behaves more
+#' consistently to equality and comparison operators. Like the latter, and
+#' like the other operators described here it preserves the dimensions of `x`
+#' and the missing values. (see examples).
+#'
+#' `%in()%`, `%in[]%`, `%in(]%` and `%in[)%` compute a `range()` from the
+#' `interval` argument and test the inclusion of `x` in this range.
 #'
 #' \code{interval} can be specified as a numeric vector of any length.
 #' In such cases \code{range} will be used in order to obtain the endpoints.
@@ -21,12 +26,15 @@
 #' @param x vector or array of values to be matched.
 #' @param table vector or list to be matched against.
 #' @param interval numeric vector defining a range to be matched against.
-#' @param pattern character string containing regular expression to be matched.
+#' `as.numeric()` is used on lists, it will succeed on flat lists but fail on
+#' nested lists
+#' @param pattern pattern to be matched against.
+#' @param value replacement value.
 #'
-#' @return a logical vector or an array of the same dimensions as \code{x}
-#' indicating if each value of \code{x} is within the defined subset.
+#' @return a logical vector or an array of the same dimensions as `x`
+#' indicating if each value of `x` is within the defined subset.
 #'
-#' @seealso \code{\%in\%}
+#' @seealso `%in%`
 #' @name in_check
 NULL
 
