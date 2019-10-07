780e8a3abaf97eb8e885c46bc42e35bad4c64fb5
moodymudskipper <antoine.fabri@gmail.com>
2019-10-07 23:47:27 +0200

    100% coverage for in_subset ops

---
+++ tests/testthat/test-in_subset.R
@@ -0,0 +1,26 @@
+test_that("subset ops work", {
+  expect_equal(1:3 %[in% c(2,4), 2)
+  expect_equal(1:3 %[in{}% c(2,4), 2)
+  expect_equal(1:3 %[in()% c(2,4), 3)
+  expect_equal(1:3 %[in[)% c(2,4), 2:3)
+  expect_equal(1:3 %[in(]% c(2,4), 3)
+  expect_equal(1:3 %[in[]% c(2,4), 2:3)
+  expect_equal(1:3 %[out% c(2,4), c(1,3))
+  expect_equal(1:3 %[out{}% c(2,4), c(1,3))
+  expect_equal(1:3 %[out()% c(2,4), 1:2)
+  expect_equal(1:3 %[out[)% c(2,4), 1)
+  expect_equal(1:3 %[out(]% c(2,4), 1:2)
+  expect_equal(1:3 %[out[]% c(2,4), 1)
+})
+
+test_that("subset regex ops work", {
+  expect_equal(letters[1:2] %[in~% "^a", "a")
+  expect_equal(letters[1:2] %[in~p% "^a", "a")
+  expect_equal(letters[1:2] %[in~f% "a", "a")
+
+  expect_equal(letters[1:2] %[out~% "^a", "b")
+  expect_equal(letters[1:2] %[out~p% "^a", "b")
+  expect_equal(letters[1:2] %[out~f% "a", "b")
+})
+
+
